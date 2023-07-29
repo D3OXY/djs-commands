cf38df838c9d3809966edcd3251294e65bf4d419
d3oxy <ishansachu1@gmail.com>
2023-07-29 06:52:55 +0000

    Removed keepAlive option from mongoose.connect() call as it is deprecated and is now defaulted to true on Mongoose version 5.2.0 and above.

--- src/DJSCommands.ts
+++ src/DJSCommands.ts
@@ -1,17 +1,17 @@
-import { Client } from 'discord.js'
-import mongoose from 'mongoose'
-import chalk from 'chalk';
+import { Client } from "discord.js";
+import mongoose from "mongoose";
+import chalk from "chalk";
 
-import CommandHandler from "./command-handler/CommandHandler"
-import EventHandler from './event-handler/EventHandler'
-import DJSCommands, { MainConfig, Validations, Events } from '../typings'
-import Cooldowns from './utils/Cooldowns'
-import DefaultCommands from './utils/DefaultCommands'
-import FeaturesHandler from './utils/FeaturesHandler'
-import DJSLogger from './utils/DJSLogger'
+import CommandHandler from "./command-handler/CommandHandler";
+import EventHandler from "./event-handler/EventHandler";
+import DJSCommands, { MainConfig, Validations, Events } from "../typings";
+import Cooldowns from "./utils/Cooldowns";
+import DefaultCommands from "./utils/DefaultCommands";
+import FeaturesHandler from "./utils/FeaturesHandler";
+import DJSLogger from "./utils/DJSLogger";
 
 class Main {
-    private _client!: Client
+    private _client!: Client;
     private _testServers!: string[];
     private _botOwners!: string[];
     private _cooldowns: Cooldowns | undefined;
@@ -20,28 +20,30 @@ class Main {
     private _validations!: Validations;
     private _commandHandler: CommandHandler | undefined;
     private _eventHandler!: EventHandler;
-    private _isConnectedToDB = false
+    private _isConnectedToDB = false;
     private _defaultPrefix!: string;
 
-    private DJSLogger: DJSLogger
+    private DJSLogger: DJSLogger;
 
     constructor(mainConfig: MainConfig) {
-        this.DJSLogger = new DJSLogger()
-        this.init(mainConfig)
+        this.DJSLogger = new DJSLogger();
+        this.init(mainConfig);
     }
 
     private async init(mainConfig: MainConfig) {
         //chalk Start
-        const name = chalk.bold(chalk.cyan('D3OXY'))
-        const lines = chalk.yellow("############################################")
-        const line = chalk.yellow("#")
-        const Title = chalk.red(chalk.underline(chalk.bold("DJS Commands")))
-        let firstLine = `${lines}`
-        let secondLine = `${line}               ${Title}               ${line}`
-        let thirdLine = `${line}                                          ${line}`
-        let fourthLine = `${line}           The Bot Has Started!           ${line}`
-        let fifthLine = `${line}          Made with ❤️  by ${name}.          ${line}`
-        let sixthLine = `${lines}`
+        const name = chalk.bold(chalk.cyan("D3OXY"));
+        const lines = chalk.yellow(
+            "############################################"
+        );
+        const line = chalk.yellow("#");
+        const Title = chalk.red(chalk.underline(chalk.bold("DJS Commands")));
+        let firstLine = `${lines}`;
+        let secondLine = `${line}               ${Title}               ${line}`;
+        let thirdLine = `${line}                                          ${line}`;
+        let fourthLine = `${line}           The Bot Has Started!           ${line}`;
+        let fifthLine = `${line}          Made with ❤️  by ${name}.          ${line}`;
+        let sixthLine = `${lines}`;
         console.log(`
                 ${firstLine}
                 ${secondLine}
@@ -49,7 +51,7 @@ class Main {
                 ${fourthLine}
                 ${fifthLine}
                 ${sixthLine}
-        `)
+        `);
         //chalk End
 
         const {
@@ -64,109 +66,129 @@ class Main {
             disabledDefaultCommands = [],
             events = {},
             validations = {},
-            defaultPrefix = '!'
-        } = mainConfig
+            defaultPrefix = "!",
+        } = mainConfig;
 
         if (!client) {
             // logToConsole(chalk.red())
-            this.DJSLogger.error("A Discord Client is required to use DJSCommands.", true);
+            this.DJSLogger.error(
+                "A Discord Client is required to use DJSCommands.",
+                true
+            );
         }
 
         if (mongoUri) {
             await this.connectToMongo(mongoUri).then(() => {
-                this.DJSLogger.success("Connected to MongoDB")
-            })
+                this.DJSLogger.success("Connected to MongoDB");
+            });
         } else {
-            this.DJSLogger.warn("No MongoDB URI provided. Any features that require a database will not function properly.")
+            this.DJSLogger.warn(
+                "No MongoDB URI provided. Any features that require a database will not function properly."
+            );
         }
 
-        this._client = client
+        this._client = client;
 
         testServers.forEach((serverId) => {
             if (this._client.guilds.cache.has(serverId)) return;
-            this.DJSLogger.warn(`The Guild ID "${serverId}" is not a valid Guild ID. Please check your config.`)
-            testServers.splice(testServers.indexOf(serverId), 1)
-        })
+            this.DJSLogger.warn(
+                `The Guild ID "${serverId}" is not a valid Guild ID. Please check your config.`
+            );
+            testServers.splice(testServers.indexOf(serverId), 1);
+        });
 
         this._testServers = testServers;
-        this._botOwners = botOwners
-        this._disableAllDefaultCommands = disableAllDefaultCommands
-        this._disabledDefaultCommands = disabledDefaultCommands
-        this._validations = validations
-        this._defaultPrefix = defaultPrefix
+        this._botOwners = botOwners;
+        this._disableAllDefaultCommands = disableAllDefaultCommands;
+        this._disabledDefaultCommands = disabledDefaultCommands;
+        this._validations = validations;
+        this._defaultPrefix = defaultPrefix;
 
         this._cooldowns = new Cooldowns(this as unknown as DJSCommands, {
-            errorMessage: 'Please wait {TIME} before doing that again.',
+            errorMessage: "Please wait {TIME} before doing that again.",
             botOwnersBypass: false,
             dbRequired: 300, // 5 minutes
-            ...cooldownConfig
-        })
-
+            ...cooldownConfig,
+        });
 
         if (commandDir) {
-            this._commandHandler = new CommandHandler(this as unknown as DJSCommands, commandDir, client)
+            this._commandHandler = new CommandHandler(
+                this as unknown as DJSCommands,
+                commandDir,
+                client
+            );
         } else {
-            this.DJSLogger.warn("No command directory provided. No commands will be loaded.")
+            this.DJSLogger.warn(
+                "No command directory provided. No commands will be loaded."
+            );
         }
 
         if (featuresDir) {
-            new FeaturesHandler(this as unknown as DJSCommands, featuresDir, client)
+            new FeaturesHandler(
+                this as unknown as DJSCommands,
+                featuresDir,
+                client
+            );
         }
 
-        this._eventHandler = new EventHandler(this as unknown as DJSCommands, events as Events, client)
+        this._eventHandler = new EventHandler(
+            this as unknown as DJSCommands,
+            events as Events,
+            client
+        );
     }
 
     public get client() {
-        return this._client
+        return this._client;
     }
 
     public get testServers() {
-        return this._testServers
+        return this._testServers;
     }
 
     public get botOwners() {
-        return this._botOwners
+        return this._botOwners;
     }
 
     public get cooldowns() {
-        return this._cooldowns
+        return this._cooldowns;
     }
 
     public get disableAllDefaultCommands() {
-        return this._disableAllDefaultCommands
+        return this._disableAllDefaultCommands;
     }
 
     public get disabledDefaultCommands() {
-        return this._disabledDefaultCommands
+        return this._disabledDefaultCommands;
     }
 
     public get commandHandler() {
-        return this._commandHandler
+        return this._commandHandler;
     }
 
     public get eventHandler() {
-        return this._eventHandler
+        return this._eventHandler;
     }
 
     public get validations() {
-        return this._validations
+        return this._validations;
     }
 
     public get isConnectedToDB(): boolean {
-        return this._isConnectedToDB
+        return this._isConnectedToDB;
     }
 
     public get defaultPrefix() {
-        return this._defaultPrefix
+        return this._defaultPrefix;
     }
 
     private async connectToMongo(mongoUri: string) {
         await mongoose.connect(mongoUri, {
-            keepAlive: true
-        })
+            // keepAlive: true,
+        });
 
-        this._isConnectedToDB = true
+        this._isConnectedToDB = true;
     }
 }
 
-export default Main
+export default Main;
