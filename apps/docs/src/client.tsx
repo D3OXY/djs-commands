1193a106883f81460932c69a739b9041bb911138
Ishan <ishansachu1@gmail.com>
2026-05-04 22:55:54 +0530

    Slice 15: apps/docs Fumadocs + TanStack Start (#71)

---
+++ apps/docs/src/client.tsx
@@ -0,0 +1,6 @@
+/// <reference types="vite/client" />
+
+import { StartClient } from "@tanstack/react-start/client";
+import { hydrateRoot } from "react-dom/client";
+
+hydrateRoot(document, <StartClient />);
